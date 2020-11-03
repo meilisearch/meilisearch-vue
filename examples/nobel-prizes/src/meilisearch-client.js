bf25df3a84e4a28a3b7e59f96bb5fb6be7ff03bb
Charlotte Vermandel <charlottevermandel@gmail.com>
2020-11-03 15:40:16 +0100

    update_key

--- examples/nobel-prizes/src/meilisearch-client.js
+++ examples/nobel-prizes/src/meilisearch-client.js
@@ -1,5 +1,5 @@
 const MEILISEARCH_HOST = "https://prizes-search.meilisearch.com"
-const MEILISEARCH_API_KEY = "Mjk0Y2MyMzZkMWQxOGQ3YTIyMGFiZDEy"
+const MEILISEARCH_API_KEY = "4ed84743870ee2ede19ea8cc9a88841bc39a6d328912f6bc7e31b616167ba404"
 
 export {
   MEILISEARCH_HOST,
