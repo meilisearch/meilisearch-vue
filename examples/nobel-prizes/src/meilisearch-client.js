c054e9d57dbf858cd22998d12631270d84269476
Charlotte Vermandel <charlottevermandel@gmail.com>
2020-11-04 16:10:32 +0100

    Update credentials of meilisearch instance

--- examples/nobel-prizes/src/meilisearch-client.js
+++ examples/nobel-prizes/src/meilisearch-client.js
@@ -1,5 +1,5 @@
-const MEILISEARCH_HOST = "https://prizes-search.meilisearch.com"
-const MEILISEARCH_API_KEY = "4ed84743870ee2ede19ea8cc9a88841bc39a6d328912f6bc7e31b616167ba404"
+const MEILISEARCH_HOST = "https://finding-demos.meilisearch.com/"
+const MEILISEARCH_API_KEY = "2b902cce4f868214987a9f3c7af51a69fa660d74a785bed258178b96e3480bb3"
 
 export {
   MEILISEARCH_HOST,
