fd20a19f4a90cc155fa6574a550fa0b90800456c
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-03-16 14:15:14 +0100

    Remove examples directory

--- examples/nobel-prizes/src/meilisearch-client.js
+++
@@ -1,7 +0,0 @@
-const MEILISEARCH_HOST = "https://demos.meilisearch.com"
-const MEILISEARCH_API_KEY = "dc3fedaf922de8937fdea01f0a7d59557f1fd31832cb8440ce94231cfdde7f25"
-
-export {
-  MEILISEARCH_HOST,
-  MEILISEARCH_API_KEY
-}
